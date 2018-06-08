35c5c8b9f18864471f139d50d86bb317c7d354a4
gc <gc22331@hotmail.com>
2018-06-08 12:55:30 +0800

    Initial commit

---
+++ pinyougousellergoodsinterface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,16 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * 品牌服务层接口
+ */
+public interface BrandService {
+    /**
+     * 查询所有品牌
+     * @return
+     */
+    List<TbBrand> findAll();
+}
